c80c0f3fd6b34ec305770de42442f2fc19da0347
naixin9393 <naicinchen@gmail.com>
2024-11-14 20:04:55 +0000

    add texture

--- public/main.js
+++ public/main.js
@@ -1,8 +1,7 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 init();
-
 
 function init() {
     const minLatitude = -50.29;
@@ -12,27 +11,27 @@ function init() {
 
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
     );
-    
+
     camera.position.z = 5;
 
-    const renderer = new THREE.WebGLRenderer();
+ 	const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-  
+
     let cameraControls = new OrbitControls(camera, renderer.domElement);
-    
-    plane(0, 0, 0, 10, 10, scene);
+
+    plane(0, 0, 0, 6.5, 5, scene);
 
     loop(scene, camera, renderer);
 }
 
 function loop(scene, camera, renderer) {
-    requestAnimationFrame(loop);
+    requestAnimationFrame(() => loop(scene, camera, renderer));
     renderer.render(scene, camera);
 }
 
@@ -42,5 +41,16 @@ function plane(x, y, z, width, height, scene) {
     const plane = new THREE.Mesh(geometry, material);
     plane.position.set(x, y, z);
     scene.add(plane);
+	loadTexture("assets/europe-asia.png", plane, width, height);
 }
 
+function loadTexture(url, map, mapWidth, mapHeight) {
+    return new THREE.TextureLoader().load(
+        url,
+
+        function (texture) {
+            map.material.map = texture;
+            map.material.needsUpdate = true;
+        }
+    );
+}
